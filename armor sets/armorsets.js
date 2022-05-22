5b89db674156c63b8ebcdb63a5ea21bde0ae2293
Lopilody <85172431+Lopilody@users.noreply.github.com>
2022-05-22 16:05:07 +0200

    sgeseg

--- armor sets/armorsets.js
+++ armor sets/armorsets.js
@@ -15,83 +15,18 @@
         window.setInterval(changeImage, 10000);
     }
 
-
-    /*
-    let searchename = [];
-    function onLoad() {
-        let gr = document.currentScript;
-        searchename[searchename.length] = gr.textContent;
-    }
-    console.log(searchename);
-
     function myFunction() {
-        let textoutput = document.getElementsByClassName('input').textoutput;
-        let namegrid = [
-
-            "Ashen Hunter set",
-            "",
-            "Armor searche",
-            "Black Church set",
-            "Butcher set",
-            "Bardor set",
-            "Bone Ash set",
-            "Cainhurst set",
-            "Charred Hunter set",
-            "Choir set",
-            "Constable set",
-            "Decorative Old Hunter set",
-            "Doll set",
-            "Executinoer set",
-            "Foreign set",
-            "Gascoingne's set",
-            "Gehrman's Hunter set",
-            "Graveguard set",
-            "Harrowed set",
-            "Henryk's Hunter set",
-            "Hunter set",
-            "Knight's set",
-            "Madman set",
-            "Maria Hunter set",
-            "Old Hunter set",
-            "Shabby set",
-            "Student set",
-            "Tomb Prospector set",
-            "White Church set",
-            "Yahar'gul set",
-            "Yamamura set"
-        ];
-        for (let index = 0; index < namegrid.length; index++) {
-            if (textoutput == namegrid[index] || textoutput == namegrid[index].toLowerCase) {
-                console.log("1");
-            }
-            else {
-                console.log("0");
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+    window.onclick = function (event) {
+        if (event.target.matches('.fa-solid fa-bars')) {
+            let dropdowns = document.getElementsByClassName("dropdown-content");
+            let i;
+            for (i = 0; i < dropdowns.length; i++) {
+                let openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
             }
         }
     }
-    let container = document.getElementsByClassName('container');
-
-
-    function Information() {
-        let rightsidetable = document.getElementsByClassName('.rightside');
-        rightsidetable.style.display = "none";
-        let rightsidetext = document.getElementsByClassName('.rightsidetext');
-        rightsidetext.style.display = "flex";
-    }
-
-    /*function myFunction() {
-   let input, filter, container, griditem, name, a, i, txtValue;
-   input = document.getElementsByClassName("searche");
-   filter = input.value.toUpperCase();
-   container = document.getElementsByClassName("container");
-   griditem = container.getElementsByTagName("div");
-   for (i = 0; i < li.length; i++) {
-       a = griditem[i].getElementsByTagName("a")[0];
-       txtValue = a.textContent || a.innerText;
-       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-           griditem[i].style.display = "";
-       } else {
-           griditem[i].style.display = "none";
-       }
-   }
-}*/
